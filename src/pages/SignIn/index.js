45b3feec5afbb3a21bab83bce51cc160725f92bd
Nursandy Ihksan <nursandyihksan97@gmail.com>
2023-03-17 10:57:45 +0700

    login page integration

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,8 @@
-import Axios from 'axios';
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import {useDispatch} from 'react-redux';
 import {Header, TextInput, Button, Gap} from '../../components';
+import {signInAction} from '../../redux/action';
 import {useForm} from '../../utils';
 
 const SignIn = ({navigation}) => {
@@ -10,15 +11,10 @@ const SignIn = ({navigation}) => {
     password: '',
   });
 
+  const dispatch = useDispatch();
+
   const onSubmit = () => {
-    console.log('form', form);
-    Axios.post('http://foodmarket-backend.buildwithangga.id/api/login', form)
-      .then(res => {
-        console.log('success', res);
-      })
-      .catch(err => {
-        console.log('error', err);
-      });
+    dispatch(signInAction(form, navigation));
   };
 
   return (
